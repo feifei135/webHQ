266228a64c764961731cff1de1b85a4bbc06e142
gaogao3 <1203582192@qq.com>
2017-11-22 15:18:20 +0800

    更新请求参数以及合并请求id

--- js/common.js
+++ js/common.js
@@ -40,8 +40,4 @@
         width:"70px",
         borderB:"0"
     };
-    $.fn.showList = function(options,params){
-        options = $.extend({},$.fn.showList.defaults,options);
-        $this = $(this);
-    };
 })(jQuery, window, document);
--- js/kline21.js
+++ js/kline21.js
@@ -21,7 +21,8 @@ var KLineSet = {
 	start: 0
 }
 var socket;
-// var ws;
+// var requireObj;
+var ws;
 (function($){
 
 	// 初始化请求参数
@@ -98,7 +99,125 @@ var socket;
 		this.defaults.HistoryKQAll = $.extend({}, this.defaults.HistoryKQAll, historyQAll);
 		this.options = $.extend({}, this.defaults, option);
 	}
-	
+
+
+	// 请求数据
+	$.KLineRequire.prototype = {
+		// websocket请求连接
+		WebSocketConnect: 	function (requireObj) {	
+								this.ws = null;
+								var lockReconnect = false,
+									timeout = 60000,//60秒
+								    timeoutObj = null,
+								    serverTimeoutObj = null,
+								    _target = this;
+								    
+								//建立socket连接
+								this.createWebSocket = function () {
+								    try {
+								        this.ws = new WebSocket(requireObj.wsUrl);
+								        return this.ws;
+								    } catch (e) {
+								        this.reconnect(requireObj.wsUrl); //如果失败重连
+								    }
+								};
+								//socket重连
+								this.reconnect = function (wsUrl) {
+								    if (lockReconnect) return;
+								    lockReconnect = true;
+								    //没连接上会一直重连，设置延迟避免请求过多
+								    setTimeout(function () {
+								        var ws = _target.createWebSocket(wsUrl);
+
+								        requireObj.initEvent(ws, requireObj);
+
+								        lockReconnect = false;
+								        console.log("重连中……");
+								    }, 2000);
+								};
+								//发送请求
+								this.request = function (data) {
+								    this.ws.send(JSON.stringify(data));
+								};
+								//重置心跳包
+								this.reset = function () {
+								    clearTimeout(this.timeoutObj);
+								    clearTimeout(this.serverTimeoutObj);
+								    return this;
+								};
+								//开始心跳包
+								this.start = function () {
+								    var self = this;
+								    this.timeoutObj = setTimeout(function () {
+								        //这里发送一个心跳，后端收到后，返回一个心跳消息，
+								        // onmessage拿到返回的心跳就说明连接正常
+								        self.request(requireObj.HeartSend);
+								        self.serverTimeoutObj = setTimeout(function () {//如果超过一定时间还没重置，说明后端主动断开了
+								            self.ws.close();//如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
+								        }, timeout)
+								    }, timeout)
+								};
+							},
+		// websocket应答				
+		initEvent: 	function(ws, requireObj) {
+						var klineOpt = this?this:"";
+						ws.onclose = function () {
+							console.log("终端重连……");
+						    socket.reconnect(requireObj.wsUrl); //终端重连
+						};
+						ws.onerror = function () {
+							console.log("报错重连……");
+						    socket.reconnect(requireObj.wsUrl); //报错重连
+						};
+						ws.onopen = function () {
+							console.log("open");
+						    //心跳检测重置
+						    socket.reset().start(); 				// 第一次建立连接则启动心跳包
+							
+							socket.request(requireObj.HistoryKQAll);
+						};
+						ws.onmessage = function (evt) {
+
+							console.log("打开成功");
+
+						    var jsons  = evt.data.split("|");  //每个json包结束都带有一个| 所以分割最后一个为空
+						    $.each(jsons,function (i,o) {
+						        if(o!==""){
+						            var data = eval("(" + o + ")");
+						            var dataList = data.d?data.d:data;
+						            var MsgType =  data["MsgType"] || data[0]["MsgType"]; //暂时用他来区分推送还是历史数据 如果存在是历史数据,否则推送行情
+						            switch(MsgType)
+						            {
+						            	case "Q619":       // 订阅快照
+						            		if(KLineData.lineType!="mline"){
+												KCharts(dataList);
+											}else{
+												return;
+											}
+						            		break;
+						                case "Q213":        // 订阅分钟线应答
+							                KCharts(dataList);
+						                    break;
+						                case "R213":        // 分钟K线历史数据查询
+						                 	socket.request(klineOpt.KQAll);	 	// 订阅当前日期K线=分钟K线
+						                 	KCharts(dataList, "history");
+						                 	break;
+						                case "R211":        // 日K线历史数据查询
+						                 	socket.request(klineOpt.KKZQAll);	 // 订阅当前日期K线=快照
+						                 	KCharts(dataList, "history");
+						                    break;    
+						                case "R646":  //心跳包
+						                    console.log(data);
+						                default:
+						            }
+						        }
+						    });
+						    //如果获取到消息，心跳检测重置
+						    //拿到任何消息都说明当前连接是正常的
+						    socket.reset().start();
+						};
+					}
+	}
 	/*
 	 * K线图构造函数
 	 */ 
@@ -795,14 +914,10 @@ var socket;
 			}else{
 				if(turnOn){
 					// Websocket实例
-					socket = new WebSocketConnect(requireObj);
-					// 建立websocket连接
-				    var ws = socket.createWebSocket();
+				    socket = new requireObj.WebSocketConnect(requireObj);
+				    ws = socket.createWebSocket();
 				    // 发起websocket请求
-				    WebSocketResponse.call(ws);
-				    // WebSocketResponse.call(ws);
-				    // var a = new wr();
-				    // requireObj.initEvent(ws);
+				    requireObj.initEvent(ws, requireObj);
 				    turnOn = false;
 				}else{
 					// 取消之前的订阅
